61f08107c6f3903dad48a93e5a37217fd43cb8b2
Julipa2023 <julianosdiogo@gmail.com>
2023-10-26 17:30:06 -0300

    .

--- main.py
+++
@@ -1,8 +0,0 @@
-from Classes import *
-
-funcionario = Programador("Carlos")
-equipamento = Computador("Avell","intel core I5")
-aparelho = Cell("samsung", "S22 Ultra")
-
-funcionario.setferramenta(equipamento)
-print(funcionario.getFerramenta().getmarca())
